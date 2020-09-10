b8817fe3735f00de6b59a8381c70227ef46c1b08
ReadGh <66055564+ReadGh@users.noreply.github.com>
2020-09-10 13:25:36 -0300

    Update date.js

--- date.js
+++ date.js
@@ -1,4 +1,5 @@
 var today = new Date();
 document.write(today);
-
+var dt = new Date();
+document.getElementById("datetime").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
 
